d949eb22388cf1b0a88f4ce31cf760e49bf86b6a
StevenHosper <stevenhosper@gmail.com>
2023-03-07 11:07:07 +0100

    Start for data functions

--- getData.py
+++ getData.py
@@ -10,23 +10,5 @@ import configuration
 import numpy as np
 
 class getData():
-    # Create useful single value arrays
-    # Zero, for empty cells or unincluded variables
-    zero = lfr.create_array(configuration.arrayShape,
-                            configuration.partitionShape,
-                            dtype = np.dtype(np.float32),
-                            fill_value = 0,
-                            )
-    # One, for additions
-    ones = lfr.create_array(configuration.arrayShape,
-                            configuration.partitionShape,
-                            dtype=np.float32,
-                            fill_value=1,
-                            )
-        
-        # For the ldd direction being towards the cell itself (pit)
-    sink = lfr.create_array(configuration.arrayShape,
-                            configuration.partitionShape,
-                            dtype = np.dtype(np.uint8),
-                            fill_value = 5,
-                            )
+    def initial():
+        pass
