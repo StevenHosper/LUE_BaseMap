b4b9e671456e4e53eb79c50eef7566cec1064bd3
StevenHosper <stevenhosper@gmail.com>
2023-04-18 10:07:39 +0200

    Fix

--- MakeGIF.py
+++ MakeGIF.py
@@ -3,6 +3,7 @@ import docopt
 import imageio.v2 as iio
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 import numpy as np
 import rasterio.plot
 import io
@@ -13,11 +14,10 @@ import datetime
 import configuration as config
 
 def slice_pathname(pathname, idx):
-    date = datetime.date(year = 2023, month = 2, day = 23)
+    date = datetime.date(year = 2023, month = 2, day = 24)
     if config.v2:
-        second = (idx) * config.dt 
+        second = (idx) * config.timestep
         date = f'{date + datetime.timedelta(seconds=idx)}_{second}'
-        print(date)
     else:
         date = date + datetime.timedelta(idx)
     return "{}_{}.tiff".format(pathname, date)
@@ -29,14 +29,39 @@ def read_raster(raster_pathname, idx):
 
 
 def create_animation(raster_pathname, nr_rasters, animation_pathname, vmin, vmax):
-    with iio.get_writer(animation_pathname, mode="i", fps=3) as writer:
+    with iio.get_writer(animation_pathname, mode="i", fps=10) as writer:
         for i in range(nr_rasters + 1):
-            figure, axis = plt.subplots(figsize=(5, 5))
+            figure, axis = plt.subplots(figsize=(10, 10))
             axis.set_axis_off()
+            data = read_raster(raster_pathname, i)
             image = rasterio.plot.show(
-                read_raster(raster_pathname, i),
+                data,
                 ax=axis,
-                cmap="hot",
+                cmap="magma",
+                norm = colors.LogNorm(vmin, vmax),
+            )
+
+            with io.BytesIO() as buffer:
+                figure.savefig(buffer, format="raw")  # , bbox_inches="tight")
+                buffer.seek(0)
+                data = np.frombuffer(buffer.getvalue(), dtype=np.uint8)
+                nr_cols, nr_rows = figure.canvas.get_width_height()
+                image = data.reshape(nr_rows, nr_cols, -1)
+
+            writer.append_data(image)
+            plt.close()
+            print(f'Processed {i} out of {nr_rasters} rasters.')
+            
+def create_animation2(raster_pathname, nr_rasters, animation_pathname, vmin, vmax):
+    with iio.get_writer(animation_pathname, mode="i", fps=10) as writer:
+        for i in range(nr_rasters + 1):
+            figure, axis = plt.subplots(figsize=(10, 10))
+            axis.set_axis_off()
+            data = read_raster(raster_pathname, i)
+            image = rasterio.plot.show(
+                data,
+                ax=axis,
+                cmap="magma",
                 vmin = vmin,
                 vmax = vmax,
             )
@@ -50,6 +75,7 @@ def create_animation(raster_pathname, nr_rasters, animation_pathname, vmin, vmax
 
             writer.append_data(image)
             plt.close()
+            print(f'Processed {i} out of {nr_rasters} rasters.')
 
 
 usage = """\
@@ -72,36 +98,36 @@ def main():
     
     # Implemenet directories for both work and home adress.
     at_work = False
-    variable = 'groundwater'
+    variable = 'discharge'
     if at_work:
         raster_pathname = f'{root_path}/output/{config.scenario}/{variable}'
         animation_pathname = f'{root_path}/output/{config.scenario}/{variable}.gif'
     else:
-        raster_pathname = f'C:/Users/steven.hosper/Desktop/Mapje Stage/output/{config.scenario}/{variable}'
+        raster_pathname = f'C:/Users/steven.hosper/Desktop/Mapje Stage/output/{config.scenario}/{config.timestep}_{variable}'
         animation_pathname = f'C:/Users/steven.hosper/Desktop/Mapje Stage/output/{config.scenario}/{variable}.gif'
     
     assert not os.path.splitext(raster_pathname)[1]
 
-    nr_rasters = 14
+    nr_rasters = 599
     assert nr_rasters >= 0
     
-    create_animation(raster_pathname, nr_rasters, animation_pathname, 10, 40)
+    create_animation(raster_pathname, nr_rasters, animation_pathname, 0, 0.02)
     
     # Second animation
-    variable = 'surfacewater'
+    variable = 'Sgw'
     if at_work:
         raster_pathname = f'{root_path}/output/{config.scenario}/{variable}'
         animation_pathname = f'{root_path}/output/{config.scenario}/{variable}.gif'
     else:
-        raster_pathname = f'C:/Users/steven.hosper/Desktop/Mapje Stage/output/{config.scenario}/{variable}'
+        raster_pathname = f'C:/Users/steven.hosper/Desktop/Mapje Stage/output/{config.scenario}/{config.timestep}_{variable}'
         animation_pathname = f'C:/Users/steven.hosper/Desktop/Mapje Stage/output/{config.scenario}/{variable}.gif'
     
     assert not os.path.splitext(raster_pathname)[1]
 
-    nr_rasters = 14
+    nr_rasters = 599
     assert nr_rasters >= 0
 
-    create_animation(raster_pathname, nr_rasters, animation_pathname, 0, 1)
+    create_animation2(raster_pathname, nr_rasters, animation_pathname, 0, 50)
 
 
 if __name__ == "__main__":
