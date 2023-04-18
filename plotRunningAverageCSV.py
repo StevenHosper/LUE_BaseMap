124b47ed2f90e658bad98d2b21f3699be697784e
StevenHosper <stevenhosper@gmail.com>
2023-04-18 10:08:18 +0200

    Commented out precip lines

--- plotRunningAverageCSV.py
+++ plotRunningAverageCSV.py
@@ -21,7 +21,7 @@ discharge = pd.read_csv(config.path + f'/output/{config.scenario}/maximumDischar
 discharge.head()
 
 # Creating exponential running mean for x amount of seconds (now 60)
-x = 600
+x = 60
 discharge[f'EWMA{x}'] = discharge['Discharge'].ewm(span=x).mean()
 
 discharge.head()
@@ -29,12 +29,12 @@ discharge.head()
 
 # Plotting
 plt.style.use('default')
-discharge[f'EWMA{x}'].plot(label='Discharge',
+discharge[['Discharge', f'EWMA{x}']].plot(label='Discharge',
                                           figsize=(16,8))
-plt.axvline(900, color = 'r')
-plt.axvline(1800, color = 'r')
-plt.axvline(2700, color = 'g')
-plt.axvline(3600, color = 'g')
+#plt.axvline(900, color = 'r')
+#plt.axvline(1800, color = 'r')
+#plt.axvline(2700, color = 'g')
+#plt.axvline(3600, color = 'g')
 
 
 plt.show()
