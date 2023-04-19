3ea7bbb514851b84e891408c7e8be534aa073dbc
StevenHosper <stevenhosper@gmail.com>
2023-04-19 11:00:37 +0200

    Removed old redundant functions

--- reporting.py
+++ reporting.py
@@ -9,30 +9,7 @@ import configuration as config
 
 # Reporting for the HydrologicBaseModel
 class report():
-    def v2(date, time, variables, output_path):
+    def dynamic(date, time, variables, output_path):
         for variable, data in variables.items():
             lfr.to_gdal(data, output_path + f'{config.timestep}_{variable}_{date}_{time}.tiff')
-        return 0
-    
-    
-    def static(date, variables, output_path):
-        for variable, data in variables.items():
-            lfr.to_gdal(data, output_path + f'{variable}_{date}.tiff')
-        return 0
-    
-    def dynamic(date, second, variables, fluxes, output_path):
-        if config.v2:
-            rest = second % 15
-            if rest == 0:
-                for variable, data in variables.items():
-                    lfr.to_gdal(data, output_path + f'{variable}_{date}_{second}.tiff')
-                for flux, data in fluxes.items():
-                    lfr.to_gdal(data, output_path + f'{flux}_{date}.tiff')
-            else:
-                pass
-        else:
-            for variable, data in variables.items():
-                lfr.to_gdal(data, output_path + f'{variable}_{date}.tiff')
-            for flux, data in fluxes.items():
-                lfr.to_gdal(data, output_path + f'{flux}_{date}.tiff')
         return 0
