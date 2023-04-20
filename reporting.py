1740ce214c7bba767abda54343742e33e9ce893d
StevenHosper <stevenhosper@gmail.com>
2023-04-20 09:22:45 +0200

    Fixed reporting format

--- reporting.py
+++ reporting.py
@@ -6,10 +6,12 @@ Created on Mon Mar 7 2023
 """
 import lue.framework as lfr
 import configuration as config
+import datetime
 
 # Reporting for the HydrologicBaseModel
 class report():
-    def dynamic(date, time, variables, output_path):
+    def dynamic(date, timestep, variables, output_path):
+        dateTime = date.strftime("%Y-%m-%d-%H%M")
         for variable, data in variables.items():
-            lfr.to_gdal(data, output_path + f'/{config.timestep}_{variable}_{date}_{time}.tiff')
+            lfr.to_gdal(data, output_path + "/{}_{}_{}.tiff".format(int(timestep), variable, dateTime))
         return 0
