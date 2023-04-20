f576802f62fdff5c9cdb71f77a47c3f88e46d1a4
StevenHosper <stevenhosper@gmail.com>
2023-04-20 12:07:26 +0200

    Fix

--- MakeGIF.py
+++ MakeGIF.py
@@ -18,57 +18,28 @@ class makeGIF():
     def __init__(self):
         pass
     
-    def slice_pathname(pathname, idx):
-        date = datetime.date(year = 2023, month = 2, day = 24)
-        if config.v2:
-            second = (idx) * config.timestep
-            date = f'{date + datetime.timedelta(seconds=idx)}_{second}'
-        else:
-            date = date + datetime.timedelta(idx)
-        return "{}_{}.tiff".format(pathname, date)
+    def slice_pathname(pathname, idx, date):
+        date = date + datetime.timedelta(minutes=idx)
+        dateTime = date.strftime("%Y-%m-%d-%H%M")
+        return "{}_{}.tiff".format(pathname, dateTime)
 
 
-    def read_raster(raster_pathname, idx):
-        dataset = gdal.Open(makeGIF.slice_pathname(raster_pathname, idx))
+    def read_raster(raster_pathname, idx, dateTime):
+        dataset = gdal.Open(makeGIF.slice_pathname(raster_pathname, idx, dateTime))
         return np.array(dataset.GetRasterBand(1).ReadAsArray())
 
 
-    def create_animation(raster_pathname, nr_rasters, animation_pathname, vmin, vmax):
+    def create_animation(raster_pathname, nr_rasters, animation_pathname, vmin, vmax, date):
         with iio.get_writer(animation_pathname, mode="i", fps=10) as writer:
             for i in range(nr_rasters + 1):
                 figure, axis = plt.subplots(figsize=(10, 10))
                 axis.set_axis_off()
-                data = makeGIF.read_raster(raster_pathname, i)
+                data = makeGIF.read_raster(raster_pathname, i, date)
                 image = rasterio.plot.show(
                     data,
                     ax=axis,
                     cmap="magma",
                     norm = colors.LogNorm(vmin, vmax),
-                )
-
-                with io.BytesIO() as buffer:
-                    figure.savefig(buffer, format="raw")  # , bbox_inches="tight")
-                    buffer.seek(0)
-                    data = np.frombuffer(buffer.getvalue(), dtype=np.uint8)
-                    nr_cols, nr_rows = figure.canvas.get_width_height()
-                    image = data.reshape(nr_rows, nr_cols, -1)
-
-                writer.append_data(image)
-                plt.close()
-                print(f'Processed {i} out of {nr_rasters} rasters.')
-                
-    def create_animation2(raster_pathname, nr_rasters, animation_pathname, vmin, vmax):
-        with iio.get_writer(animation_pathname, mode="i", fps=10) as writer:
-            for i in range(nr_rasters + 1):
-                figure, axis = plt.subplots(figsize=(10, 10))
-                axis.set_axis_off()
-                data = makeGIF.read_raster(raster_pathname, i)
-                image = rasterio.plot.show(
-                    data,
-                    ax=axis,
-                    cmap="magma",
-                    vmin = vmin,
-                    vmax = vmax,
                 )
 
                 with io.BytesIO() as buffer:
@@ -113,6 +84,8 @@ def run(configuration):
     vmaxDict    = dict(zip(variables, vmaxList))
     
     # Set amount of rasters and timestep
+    sD = list(map(int, configuration.modelSettings['startDate'].split(", ")))
+    startDate   = datetime.datetime(sD[0], sD[1], sD[2], sD[3], sD[4], sD[5])
     nr_rasters  = int(configuration.gifSettings['nrRasters'])
     timestep    = int(configuration.modelSettings['timestep'])
     assert nr_rasters >= 0
@@ -126,7 +99,7 @@ def run(configuration):
         vmin = vminDict[var]
         vmax = vmaxDict[var]
         
-        makeGIF.create_animation(raster_pathname, nr_rasters, animation_pathname, vmin, vmax)
+        makeGIF.create_animation(raster_pathname, nr_rasters, animation_pathname, vmin, vmax, startDate)
 
 
 if __name__ == "__main__":
