dbfe0904c0339ce26d0d0cf60e170cf6588b2a00
StevenHosper <stevenhosper@gmail.com>
2023-05-09 12:34:30 +0200

    Fixed styling

--- model/StandardArraysLUE.py
+++ model/StandardArraysLUE.py
@@ -16,43 +16,42 @@ class StandardArraysLUE():
         1) Set extent, input and output dir.
         """
         
-        self.arrayExtent    = int(configuration.modelSettings['arrayExtent'])
-        self.partitionExtent= int(configuration.modelSettings['partitionExtent'])
-        self.outputDir      = configuration.generalSettings['outputDir']
-        self.inputDir       = configuration.generalSettings['inputDir']
+        self.array_extent    = int(configuration.modelSettings['arrayExtent'])
+        self.partition_extent= int(configuration.modelSettings['partitionExtent'])
+        self.output_dir      = configuration.generalSettings['outputDir']
         
     
     def boundary_cell(self):
-        array = np.ones((self.partitionExtent - 2, self.arrayExtent - 2), dtype= np.uint8)
-        boundaryCell = np.pad(array, pad_width=1, mode='constant', constant_values=0)
-        boundaryCell = lfr.from_numpy(boundaryCell, 2*(self.partitionExtent,))
-        lfr.to_gdal(boundaryCell, self.outputDir + '/boundaryCell.tiff')
-        return boundaryCell
+        array = np.ones((self.partition_extent - 2, self.array_extent - 2), dtype= np.uint8)
+        boundary_cell = np.pad(array, pad_width=1, mode='constant', constant_values=0)
+        boundary_cell = lfr.from_numpy(boundary_cell, 2*(self.partition_extent,))
+        lfr.to_gdal(boundary_cell, self.output_dir + '/boundary_cell.tiff')
+        return boundary_cell
     
     def zero(self):
-        return lfr.create_array(2*(self.arrayExtent,),
-                                2*(self.partitionExtent,),
+        return lfr.create_array(2*(self.array_extent,),
+                                2*(self.partition_extent,),
                                 dtype = np.dtype(np.float64),
                                 fill_value = 0,
                                 )
     
     def one(self):
-        return lfr.create_array(2*(self.arrayExtent,),
-                                2*(self.partitionExtent,),
+        return lfr.create_array(2*(self.array_extent,),
+                                2*(self.partition_extent,),
                                 dtype = np.dtype(np.float64),
                                 fill_value = 1,
                                 )
 
     def one_int(self):
-        return lfr.create_array(2*(self.arrayExtent,),
-                                2*(self.partitionExtent,),
+        return lfr.create_array(2*(self.array_extent,),
+                                2*(self.partition_extent,),
                                 dtype = np.dtype(np.uint8),
                                 fill_value = 1,
                                 )
 
     def ldd_sink(self):
-        return lfr.create_array(2*(self.arrayExtent,),
-                                2*(self.partitionExtent,),
+        return lfr.create_array(2*(self.array_extent,),
+                                2*(self.partition_extent,),
                                 dtype = np.dtype(np.uint8),
                                 fill_value = 5,
                                 )
