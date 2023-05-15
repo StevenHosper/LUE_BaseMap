71633b156375d749b062385f9e6390a18d8dc961
StevenHosper <stevenhosper@gmail.com>
2023-05-15 11:01:35 +0200

    Added reporting submodule and routing methods

--- model/reporting.py
+++ model/reporting.py
@@ -18,6 +18,12 @@ class Report:
         self.timestep = configuration.modelSettings['timestep']
         self.output_dir = configuration.generalSettings['outputDir'] + configuration.generalSettings["scenario"]
         self.input_dir   = configuration.generalSettings['inputDir'] + configuration.generalSettings['scenario'] 
+    
+    def initial(self, variables):
+        for variable, data in variables.items():
+            lfr.to_gdal(data, self.output_dir + "/{}_{}.tiff".format(self.timestep,
+                                                                         variable
+                                                                         ))
         
     def dynamic(self, date, variables):
         dateTime = date.strftime("%Y-%m-%d-%H%M")
@@ -26,7 +32,6 @@ class Report:
                                                                          variable,
                                                                          dateTime
                                                                          ))
-        return 0
      
     def balance_report(self, configuration):
         start_date = self.string_to_datetime(configuration.modelSettings['startDate'], seperator= ", ")
@@ -57,40 +62,45 @@ class Report:
         
         # Load initial files, if they cannot be loaded, assume zero (same as model does)
         try:
-            ini_sur_stor = self.tiff_to_np_sum(self.input_dir + configuration.dataSettings["iniWaterHeight"])
+            ini_sur_height = self.tiff_to_np_sum(self.input_dir + configuration.dataSettings["iniWaterHeight"])
         except:
-            self.standard_LUE.zero()
+            ini_sur_height = self.tiff_to_np_sum(self.output_dir + "/{}_ini_sur_h.tiff".format(int(configuration.modelSettings["timestep"])))
+            ini_sur_height = self.tiff_to_np_sum(self.output_dir + "/{}_height_{}.tiff".format(int(configuration.modelSettings["timestep"]), start_date.strftime("%Y-%m-%d-%H%M")))
         
         try:
             ini_gro_stor = self.input_dir + configuration.dataSettings['iniGroundWaterStorage']
+            ini_gro_data = gdal.Open(ini_gro_stor)
+            img = ini_gro_data.GetRasterBand(1)
         except:
-            self.standard_LUE.zero()
+            ini_gro_stor = (self.output_dir + "/{}_ini_gw_s.tiff".format(int(configuration.modelSettings["timestep"])))
         
         try:
             ini_int_stor = self.tiff_to_np_sum(self.input_dir + configuration.dataSettings['iniInterceptionStorage'])
         except:
-            self.standard_LUE.zero()
+            ini_int_stor = self.tiff_to_np_sum(self.output_dir + "/{}_ini_int_s.tiff".format(int(configuration.modelSettings["timestep"])))
+            print(ini_int_stor)
         
-        end_sur_stor = self.tiff_to_np_sum(self.output_dir + "/{}_height_{}.tiff".format(int(configuration.modelSettings["timestep"]), end_date.strftime("%Y-%m-%d-%H%M")))
+        end_sur_height = self.tiff_to_np_sum(self.output_dir + "/{}_height_{}.tiff".format(int(configuration.modelSettings["timestep"]), end_date.strftime("%Y-%m-%d-%H%M")))
         end_gro_stor = self.output_dir + "/{}_gw_s_{}.tiff".format(int(configuration.modelSettings["timestep"]), end_date.strftime("%Y-%m-%d-%H%M"))
         end_int_stor = self.tiff_to_np_sum(self.output_dir + "/{}_int_s_{}.tiff".format(int(configuration.modelSettings["timestep"]), end_date.strftime("%Y-%m-%d-%H%M")))
         
         resolution = (int(configuration.modelSettings["resolution"]))
+        channel_area= resolution * int(configuration.modelSettings['channel_width'])
         cell_area   = resolution ** 2
         
-        del_sur_stor = (end_sur_stor - ini_sur_stor) * resolution
+        del_sur_stor = (end_sur_height - ini_sur_height) * channel_area
         del_gro_stor = self.tiff_to_np_sum_difference(end_gro_stor, ini_gro_stor) * float(configuration.modelSettings["porosity"])
         del_int_stor = (end_int_stor - ini_int_stor)
         net_balance = del_sur_stor + del_int_stor + del_gro_stor
-        precipitation       = (((end_idx - start_idx) / 12) * mean_precipitation) / 1000 * cell_area * (int(configuration.modelSettings["arrayExtent"]) ** 2) * (float(configuration.modelSettings["validCellsPercentage"]))/100
-        evapotranspiration  = (((end_idx - start_idx) / 12) * mean_evapotranspiration) / 1000 * cell_area * (int(configuration.modelSettings["arrayExtent"]) ** 2) * (float(configuration.modelSettings["validCellsPercentage"]))/100
+        precipitation       = (((end_idx - start_idx) / 12) * mean_precipitation) / 1000 * cell_area * (int(configuration.modelSettings["arrayExtent"]) ** 2) * (float(configuration.modelSettings["validCellsPercentageG"]))/100
+        evapotranspiration  = (((end_idx - start_idx) / 12) * mean_evapotranspiration) / 1000 * cell_area * (int(configuration.modelSettings["arrayExtent"]) ** 2) * (float(configuration.modelSettings["validCellsPercentageG"]))/100
         atmospheric_balance = precipitation - evapotranspiration
         
         print("total precipitation:               ", precipitation, "m3")
         print("total evapotranspiration:          ", evapotranspiration, "m3 \n")
         
-        print("iniTotalSurfaceHeight:             ", ini_sur_stor * resolution, "m3")
-        print("endTotalSurfaceHeight:             ", end_sur_stor * resolution, "m3")
+        print("iniTotalSurfaceHeight:             ", ini_sur_height * channel_area, "m3")
+        print("endTotalSurfaceHeight:             ", end_sur_height * channel_area, "m3")
         print("delta Surface Storage:             ", del_sur_stor, "m3 \n")
         
         print("delta GroundWater Storage:         ", del_gro_stor, "m3 \n")
@@ -113,13 +123,10 @@ class Report:
         return 0
     
     def tiff_to_np_sum(self, file):
-        try:
-            data = gdal.Open(file)
-            img = data.GetRasterBand(1)
-            raster = img.ReadAsArray()
-            np_array_sum = np.nansum(raster)
-        except:
-            np_array_sum = 0
+        data = gdal.Open(file)
+        img = data.GetRasterBand(1)
+        raster = img.ReadAsArray()
+        np_array_sum = np.nansum(raster)
         return np_array_sum
     
     def string_to_datetime(self, date_string: str, seperator: str):
@@ -134,14 +141,15 @@ class Report:
         
     
     def tiff_to_np_sum_difference(self, file1, file2):
-        try:
-            data1 = gdal.Open(file1)
-            img1 = data1.GetRasterBand(1)
-            raster1 = img1.ReadAsArray()
-            data2 = gdal.Open(file2)
-            img2 = data2.GetRasterBand(1)
-            raster2 = img2.ReadAsArray()
-            np_array_sum = np.nansum(raster1 - raster2)
-        except:
-            np_array_sum = 0
+        data1 = gdal.Open(file1)
+        img1 = data1.GetRasterBand(1)
+        raster1 = img1.ReadAsArray()
+
+        data2 = gdal.Open(file2)
+        img2 = data2.GetRasterBand(1)
+        raster2 = img2.ReadAsArray()
+        
+        np_array_sum = np.nansum(raster1 - raster2)
+
+  
         return np_array_sum
